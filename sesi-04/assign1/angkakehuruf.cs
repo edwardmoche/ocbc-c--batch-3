fddfc9eb6f616f244b4c8be2f8615d0621f87392
edwardmoche <edwardmoche@yahoo.com>
2022-03-22 16:04:30 +0700

    tambah data baru

---
+++ sesi-04/assign1/angkakehuruf.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class numtochar{
+    static void Main(){  
+            int[,] n ={
+                {1, 1},
+                {2, 4},
+                {3, 9},
+                {4, 16},
+                {5, 25},
+                {6, 36},
+                {7, 49},
+                {8, 64},
+                {9, 81},
+                {10, 100}
+            };
+                int i, j;
+
+                for (i=0; i< 10; i++){
+                    for(j=0; j<2; j++){
+                        Console.Write(n[" ",j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+    }
+}
---
+++ sesi-04/string3/string3.cs
@@ -0,0 +1,20 @@
+using System;  
+    public class string3{  
+        static void Main(){  
+            string[] str = {"ini","adalah","sebuah","test."};
+            Console.WriteLine("Array asli: ");
+
+            for(int i=0; i<str.Length; i++){
+                Console.Write(str[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            str[1] = "Merupakan";
+            str[3] = "test, juga!";
+            Console.WriteLine("Array termodifikasi: ");
+
+            for (int i=0; i<str.Length; i++){
+                Console.Write(str[i] + " ");
+            }
+    }  
+} 
